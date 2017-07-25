ab065ab8ead891a12b1b3d728085131b4fe9d90a
amusarra <antonio.musarra@gmail.com>
2017-07-25 14:07:00 +0200

    Add Dummy interface

---
+++ src/main/java/it/dontesta/labs/liferay/salesforce/client/soap/DummySalesforceClient.java
@@ -0,0 +1,15 @@
+package it.dontesta.labs.liferay.salesforce.client.soap;
+
+/**
+ * A dummy client Salesforge interface
+ *
+ * @author Antonio Musarra antonio.musarra@gmail.com
+ */
+public interface DummySalesforceClient {
+  /**
+   * A default dummy method
+   *
+   * @return a dummy return
+   */
+  public int myDummyMethod();
+}
